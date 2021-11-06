4bfda3d8c96b860d3b433fe93ed09225d56ff0c4
zassou65535 <gravity@kuf.biglobe.ne.jp>
2021-11-06 15:07:12 +0900

    変換前後における比較画像を出力できるようにする

--- module/inference.py
+++ module/inference.py
@@ -24,9 +24,9 @@ import torchaudio
 #任意のframe長のspectrogramに対応
 def inference(input_spectrogram, netG, unit_frame=160, cutout_frame=128):
 	device = input_spectrogram.device
-	#input_spectrogram : torch.Size([frequency, frame])
-	frequency = input_spectrogram.size()[0]
-	frame = input_spectrogram.size()[1]
+	#input_spectrogram : torch.Size([..., frequency, frame])
+	frequency = input_spectrogram.size()[-2]
+	frame = input_spectrogram.size()[-1]
 	padding_frame = (unit_frame - cutout_frame)//2
 	#cutout_frameフレームずつ変換を行う　unit_frameずつinput_spectrogramから取り出しnetGで変換、出力の中央cutout_frameフレームを結果とする
 	result_segments = []#変換結果を格納
@@ -35,21 +35,66 @@ def inference(input_spectrogram, netG, unit_frame=160, cutout_frame=128):
 		start_frame = i*cutout_frame - padding_frame
 		end_frame = (i+1)*cutout_frame + padding_frame
 		#指定の箇所を抽出
-		target_segment = input_spectrogram[:, max(0, start_frame):min(frame, end_frame)]
+		target_segment = input_spectrogram[..., max(0, start_frame):min(frame, end_frame)]
 		#足りない分に関してzero paddingを行う
 		if(start_frame<0):
-			target_segment = torch.cat([torch.zeros(frequency, -start_frame).to(device), target_segment], dim=-1)
-		target_segment = torch.cat([target_segment, torch.zeros(frequency, unit_frame - target_segment.size()[1]).to(device)], dim=-1)
+			target_segment = torch.cat([torch.zeros(1, frequency, -start_frame).to(device), target_segment], dim=-1)
+		target_segment = torch.cat([target_segment, torch.zeros(1, frequency, unit_frame - target_segment.size()[1]).to(device)], dim=-1)
 		#netGを用いて変換
 		with torch.no_grad():
-			result_segment = netG(target_segment[None, ...])[0]
+			result_segment = netG(target_segment)
 		#出力の中央cutout_frameフレームを結果とする
-		result_segments.append(torch.narrow(result_segment, dim=1, start=16, length=cutout_frame))
-	result_segments = torch.cat(result_segments, dim=1)
+		result_segments.append(torch.narrow(result_segment, dim=-1, start=16, length=cutout_frame))
+	result_segments = torch.cat(result_segments, dim=-1)
 	result_segments = result_segments[..., 0:frame]
 	return result_segments
 
+#元音声(source)と変換結果(result)を、波形とスペクトログラム2つの観点で比較するためのグラフを出力する
+def output_comparison_graph(
+		save_path, 
+		waveform_source, waveform_result,      #waveform : torch.size([frame])
+		spectrogram_source, spectrogram_result, #spectrogram : torch.Size([frequency, frame]
+		sampling_rate, #サンプリングレート
+	):
+	plt.clf()
+	plt.figure(figsize=(10,5))
 
+	#スペクトログラムの描画
+	plt.subplot(2,2,1)
+	spectrogram_db = 20*torch.log10(spectrogram_source/torch.max(spectrogram_source))#dbに変換, スペクトログラムを見やすくする
+	plt.title("spectrogram_source")
+	plt.imshow(spectrogram_db)
+	plt.xlabel("frame")
+	plt.ylabel("frequency")
+	plt.gca().invert_yaxis()
+
+	plt.subplot(2,2,2)
+	spectrogram_db = 20*torch.log10(spectrogram_result/torch.max(spectrogram_result))#dbに変換, スペクトログラムを見やすくする
+	plt.title("spectrogram_result")
+	plt.imshow(spectrogram_db)
+	plt.xlabel("frame")
+	plt.ylabel("frequency")
+	plt.gca().invert_yaxis()
+
+	#波形の描画
+	plt.subplot(2,2,3)
+	num_frames = waveform_source.size()[0]
+	time_axis = torch.arange(0, num_frames) / sampling_rate
+	plt.title("waveform_source")
+	plt.plot(time_axis.tolist(), waveform_source.tolist(), linewidth=1)
+	plt.grid()
+	plt.xlabel("time")
+
+	plt.subplot(2,2,4)
+	num_frames = waveform_result.size()[0]
+	time_axis = torch.arange(0, num_frames) / sampling_rate
+	plt.title("waveform_result")
+	plt.plot(time_axis, waveform_result, linewidth=1)
+	plt.grid()
+	plt.xlabel("time")
+
+	plt.savefig(save_path)
+	plt.close()
 
 
 
--- scyclone_inference.py
+++ scyclone_inference.py
@@ -33,7 +33,7 @@ torch.manual_seed(manualSeed)
 #変換したいwavファイルへの、各データへのパスのフォーマット
 wav_path = "./dataset/train/domainA/jvs_extracted/ver1/jvs001/VOICEACTRESS100_010.wav"
 #scycloneの学習済みモデルへのパス
-scyclone_trained_model_path = "./output/scyclone/train/iteration99999/generator_A2B_trained_model_cpu.pth"
+scyclone_trained_model_path = "./output/scyclone/train/iteration25000/generator_A2B_trained_model_cpu.pth"
 #結果を出力するためのディレクトリ
 output_dir = "./output/scyclone/inference/"
 #使用するデバイス
@@ -54,19 +54,29 @@ netG = netG.to(device)
 audio_filepath_list = make_datapath_list(wav_path)
 for i, audio_filepath in enumerate(audio_filepath_list, 0):
 	#音声ファイルの読み込み
-	waveform, sample_rate = torchaudio.load(audio_filepath)
-	input_waveform = waveform.squeeze(dim=0)
+	input_waveform, _ = torchaudio.load(audio_filepath)
 	input_spectrogram = torchaudio.transforms.Spectrogram(n_fft=254, hop_length=128)(input_waveform)
 	#変換の実行
 	input_spectrogram = input_spectrogram.to(device)
 	output_spectrogram = inference(input_spectrogram=input_spectrogram, netG=netG)
+	#スペクトログラムから負の値を取り除く
+	output_spectrogram = F.relu(output_spectrogram)
 	#スペクトログラムから波形への変換
 	output_spectrogram = output_spectrogram.to("cpu")
-	output_waveform = torchaudio.transforms.GriffinLim(n_fft=254, n_iter=256, hop_length=128)(output_spectrogram)[None,...]
-	#ファイルに保存
+	output_waveform = torchaudio.transforms.GriffinLim(n_fft=254, n_iter=256, hop_length=128)(output_spectrogram)
+	# print("input:", input_waveform.size(), input_spectrogram.size())
+	# print("output:", output_waveform.size(), output_spectrogram.size())
+	#音声ファイルを保存
 	save_dir = os.path.join(output_dir, f"result_{i}")
 	os.makedirs(save_dir, exist_ok=True)#出力用ディレクトリがなければ作る
-	torchaudio.save(os.path.join(save_dir, os.path.basename(audio_filepath)), waveform, sample_rate=16000)
+	torchaudio.save(os.path.join(save_dir, os.path.basename(audio_filepath)), input_waveform, sample_rate=16000)
 	torchaudio.save(os.path.join(save_dir, "converted_"+os.path.basename(audio_filepath)), output_waveform, sample_rate=16000)
-	print(f"generated {os.path.join(save_dir, 'converted_'+os.path.basename(audio_filepath))}")
+	print(f'generated {os.path.join(save_dir, "converted_"+os.path.basename(audio_filepath))}')
+	#変換前後を比較するためのグラフを出力
+	output_comparison_graph(
+		save_path=os.path.join(save_dir, "comparison.png"), 
+		waveform_source=input_waveform.squeeze(dim=0).to("cpu"), waveform_result=output_waveform.squeeze(dim=0).to("cpu"),      #waveform : torch.size([frame])
+		spectrogram_source=input_spectrogram.squeeze(dim=0).to("cpu"), spectrogram_result=output_spectrogram.squeeze(dim=0).to("cpu"), #spectrogram : torch.Size([frequency, frame]
+		sampling_rate=16000 #サンプリングレート
+	)
 
