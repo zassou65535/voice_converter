c64aa0fb6951b6d1c5fb04b379c46794347765b5
zassou65535 <gravity@kuf.biglobe.ne.jp>
2021-11-11 00:51:41 +0900

    scycloneの推論部を改良

--- Scyclone_inference.py
+++ Scyclone_inference.py
@@ -24,18 +24,18 @@ from module.dataset import *
 from module.generator import *
 from module.vocoder import *
 
-#乱数のシードを設定　これにより再現性を確保できる
+#乱数のシードを設定
 manualSeed = 999
 print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
-#変換したいwavファイルへの、各データへのパスのフォーマット
-wav_path = "./dataset/train/domainA/jvs_extracted/ver1/jvs001/VOICEACTRESS100_010.wav"
+#変換したいwavファイルへのパス
+input_audio_path = "./dataset/train/domainA/jvs_extracted/ver1/jvs001/VOICEACTRESS100_010.wav"
 #Scycloneの学習済みGeneratorへのパス
-scyclone_trained_model_path = "./output/scyclone/train/iteration640000/generator_A2B_trained_model_cpu.pth"
+scyclone_trained_model_path = "./output/scyclone/train/iteration700000/generator_A2B_trained_model_cpu.pth"
 #WaveRNNの学習済みVocoderへのパス
-wavernn_trained_model_path = "./output/wavernn/train/iteration37500/vocoder_trained_model_cpu.pth"
+wavernn_trained_model_path = "./output/wavernn/train/iteration90000/vocoder_trained_model_cpu.pth"
 #結果を出力するためのディレクトリ
 output_dir = "./output/scyclone/inference/"
 #使用するデバイス
@@ -63,70 +63,67 @@ vocoder.load_state_dict(torch.load(wavernn_trained_model_path))
 #ネットワークをデバイスに移動
 vocoder = vocoder.to(device)
 
-#各wavファイルに対して推論を行う
-audio_filepath_list = make_datapath_list(wav_path)
-for i, audio_filepath in enumerate(audio_filepath_list, 0):
-	#音声ファイルの読み込み
-	input_waveform, _ = torchaudio.load(audio_filepath)
-	input_spectrogram = torchaudio.transforms.Spectrogram(n_fft=254, hop_length=128)(input_waveform)
+#変換対象とする音声ファイルの読み込み
+input_waveform, _ = torchaudio.load(input_audio_path)
+#読み込んだ波形からスペクトログラムを生成
+input_spectrogram = torchaudio.transforms.Spectrogram(n_fft=254, hop_length=128)(input_waveform)
+input_spectrogram = input_spectrogram.to(device)
 
-	#変換の実行
-	input_spectrogram = input_spectrogram.to(device)
-	#input_spectrogram : torch.Size([..., frequency, frame])
-	frequency = input_spectrogram.size()[-2]
-	frame = input_spectrogram.size()[-1]
-	padding_frame = (unit_frame - cutout_frame)//2
-	#cutout_frameフレームずつ変換を行う　unit_frameずつinput_spectrogramから取り出しnetGで変換、出力の中央cutout_frameフレームを結果とする
-	result_segments = []#変換結果を格納
-	for i in range(0, frame//cutout_frame):
-		#切り取る箇所を指定
-		start_frame = i*cutout_frame - padding_frame
-		end_frame = (i+1)*cutout_frame + padding_frame
-		#指定の箇所を抽出
-		target_segment = input_spectrogram[..., max(0, start_frame):min(frame, end_frame)]
-		#足りない分に関してzero paddingを行う
-		if(start_frame<0):
-			target_segment = torch.cat([torch.zeros(1, frequency, -start_frame).to(device), target_segment], dim=-1)
-		target_segment = torch.cat([target_segment, torch.zeros(1, frequency, unit_frame - target_segment.size()[1]).to(device)], dim=-1)
-		#netGを用いて変換
-		with torch.no_grad():
-			result_segment = netG(target_segment)
-		#出力の中央cutout_frameフレームを結果とする
-		result_segments.append(torch.narrow(result_segment, dim=-1, start=16, length=cutout_frame))
-	#変換されたスペクトログラムを1つのTensorにまとめる
-	result_segments = torch.cat(result_segments, dim=-1)
-	#スペクトログラムのフレーム数をinput_spectrogramと同じ長さになるよう揃える
-	output_spectrogram = result_segments[..., 0:frame]
-	#スペクトログラムから負の値を取り除く
-	output_spectrogram = F.relu(output_spectrogram)
+#input_spectrogram : torch.Size([1, frequency, frame])
+frequency = input_spectrogram.size()[-2]
+frame = input_spectrogram.size()[-1]
+#スペクトログラムをpaddingする際のサイズ
+padding_frame = (unit_frame - cutout_frame)//2
 
-	#Vocoderによるスペクトログラムから波形への変換
-	output_waveform_by_vocoder = vocoder.generate(output_spectrogram.transpose(1, 2)).to("cpu")[None, ...]
-	#GriffinLimによるスペクトログラムから波形への変換
-	output_spectrogram = output_spectrogram.to("cpu")
-	output_waveform_by_griffinlim = torchaudio.transforms.GriffinLim(n_fft=254, n_iter=256, hop_length=128)(output_spectrogram)
-	
-	#音声ファイルを保存
-	save_dir = os.path.join(output_dir, f"result_{i}")
-	os.makedirs(save_dir, exist_ok=True)#出力用ディレクトリがなければ作る
-	torchaudio.save(os.path.join(save_dir, "input_audio.wav"), input_waveform, sample_rate=16000)
-	torchaudio.save(os.path.join(save_dir, "output_audio_by_vocoder.wav"), output_waveform_by_vocoder, sample_rate=16000)
-	torchaudio.save(os.path.join(save_dir, "output_audio_by_griffinlim.wav"), output_waveform_by_griffinlim, sample_rate=16000)
-	print(f'converted {audio_filepath}')
-	#変換前後の音声を、波形とスペクトログラム2つの観点で比較するためのグラフを出力する
-	waveform_list = [
-		(input_waveform.squeeze(dim=0).to("cpu"), "input_waveform"),
-		(output_waveform_by_vocoder.squeeze(dim=0).to("cpu"), "output_waveform_by_vocoder"),
-		(output_waveform_by_griffinlim.squeeze(dim=0).to("cpu"), "output_waveform_by_griffinlim")
-	]
-	spectrogram_list = [
-		(input_spectrogram.squeeze(dim=0).to("cpu"), "input_spectrogram"),
-		(output_spectrogram.squeeze(dim=0).to("cpu"), "converted_spectrogram")
-	]
-	output_comparison_graph(
-			save_path = os.path.join(save_dir, "comparison.png"),
-			waveform_list=waveform_list,     #waveform_list : (torch.size([frame]), graph_title)を要素に持つlist
-			spectrogram_list=spectrogram_list, #spectrogram_list : (torch.Size([frequency, frame]), graph_title)を要素に持つlist
-			sampling_rate=16000 #サンプリングレート
-		)
+#cutout_frameフレームずつ変換を行う　unit_frameずつinput_spectrogramから取り出しnetGで変換、出力の中央cutout_frameフレームを結果とする
+result_segments = []#変換結果を格納
+for i in range(0, frame//cutout_frame):
+	#切り取る箇所を指定
+	start_frame = i*cutout_frame - padding_frame
+	end_frame = (i+1)*cutout_frame + padding_frame
+	#指定の箇所を抽出
+	target_segment = input_spectrogram[..., max(0, start_frame):min(frame, end_frame)]
+	#足りない分に関してzero paddingを行う
+	if(start_frame<0):
+		target_segment = torch.cat([torch.zeros(1, frequency, -start_frame).to(device), target_segment], dim=-1)
+	target_segment = torch.cat([target_segment, torch.zeros(1, frequency, unit_frame - target_segment.size()[1]).to(device)], dim=-1)
+	#netGを用いて変換
+	with torch.no_grad():
+		result_segment = netG(target_segment)
+	#出力の中央cutout_frameフレームを結果とする
+	result_segments.append(torch.narrow(result_segment, dim=-1, start=16, length=cutout_frame))
+#変換されたスペクトログラムを1つのTensorにまとめる
+result_segments = torch.cat(result_segments, dim=-1)
+#スペクトログラムのフレーム数をinput_spectrogramと同じ長さになるよう揃える
+output_spectrogram = result_segments[..., 0:frame]
+#スペクトログラムから負の値を取り除く
+output_spectrogram = F.relu(output_spectrogram)
+
+#Vocoderによる、スペクトログラムから波形への変換
+output_waveform_by_vocoder = vocoder.generate(output_spectrogram.transpose(1, 2)).to("cpu")[None, ...]
+#GriffinLimによる、スペクトログラムから波形への変換(vocoderによる生成結果との比較用)
+output_waveform_by_griffinlim = torchaudio.transforms.GriffinLim(n_fft=254, n_iter=256, hop_length=128)(output_spectrogram.to("cpu"))
+
+#音声ファイルを保存
+os.makedirs(output_dir, exist_ok=True)#出力用ディレクトリがなければ作る
+torchaudio.save(os.path.join(output_dir, "input_audio.wav"), input_waveform, sample_rate=16000)
+torchaudio.save(os.path.join(output_dir, "output_audio_by_vocoder.wav"), output_waveform_by_vocoder, sample_rate=16000)
+torchaudio.save(os.path.join(output_dir, "output_audio_by_griffinlim.wav"), output_waveform_by_griffinlim, sample_rate=16000)
+
+#変換前後の音声を、波形とスペクトログラム2つの観点で比較するためのグラフを出力する
+waveform_list = [
+	(input_waveform.squeeze(dim=0).to("cpu"), "input_waveform"),
+	(output_waveform_by_vocoder.squeeze(dim=0).to("cpu"), "output_waveform_by_vocoder"),
+	(output_waveform_by_griffinlim.squeeze(dim=0).to("cpu"), "output_waveform_by_griffinlim")
+]
+spectrogram_list = [
+	(input_spectrogram.squeeze(dim=0).to("cpu"), "input_spectrogram"),
+	(output_spectrogram.squeeze(dim=0).to("cpu"), "output_spectrogram")
+]
+output_comparison_graph(
+		save_path = os.path.join(output_dir, "comparison.png"),
+		waveform_list=waveform_list,     #waveform_list : (torch.size([frame]), graph_title)を要素に持つlist
+		spectrogram_list=spectrogram_list, #spectrogram_list : (torch.Size([frequency, frame]), graph_title)を要素に持つlist
+		sampling_rate=16000 #サンプリングレート
+	)
 
